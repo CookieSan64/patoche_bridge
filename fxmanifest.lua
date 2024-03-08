61fe76ef54d08513ff3ab473d00445b8b75d65bc
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:27:36 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+author "ChouCookieSan"
+description "Pont vers Cayo"
+version "1.0.0"
+
+fx_version "cerulean"
+game "gta5"
+
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/bridge_part.ytyp'
+
+
